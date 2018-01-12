7dde2b4c73a1133c5bbf476db4a6f4b3fb4a7c90
Aidan Heller <asfheller@gmail.com>
2018-01-11 19:00:08 -0800

    Make styles for text

--- frontend/src/main/kotlin/com/jamdan2/kotlinWebapp/main.kt
+++ frontend/src/main/kotlin/com/jamdan2/kotlinWebapp/main.kt
@@ -1,9 +1,9 @@
 package com.jamdan2.kotlinWebapp
 
-import kotlinext.js.*
 import kotlin.browser.*
+import kotlinext.js.*
 import react.dom.*
-import react.router.dom.hashRouter
+import react.router.dom.*
 
 fun main(args: Array<String>) {
     require("styles.css")
@@ -11,6 +11,27 @@ fun main(args: Array<String>) {
         render(document.getElementById("root")!!) {
             hashRouter {
                 application()
+            }
+            p {
+                +"Paragraph"
+            }
+            h1 {
+                +"Header 1"
+            }
+            h2 {
+                +"Header 2"
+            }
+            h3 {
+                +"Header 3"
+            }
+            h4 {
+                +"Header 4"
+            }
+            h5 {
+                +"Header 5"
+            }
+            h6 {
+                +"Header 6"
             }
         }
         println("Hello, world!")
