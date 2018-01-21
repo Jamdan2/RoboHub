06c8528aae71e1b36924225c2377417dc6415855
Aidan Heller <asfheller@gmail.com>
2018-01-21 15:21:15 -0800

    Handle stylesheets individually

--- frontend/src/main/kotlin/com/jamdan2/robohub/main.kt
+++ frontend/src/main/kotlin/com/jamdan2/robohub/main.kt
@@ -6,7 +6,7 @@ import react.dom.*
 import react.router.dom.*
 
 fun main(args: Array<String>) {
-    require("styles.css")
+    requireAll(require.context("/", true, js("/\\.css$/")))
     window.onload = {
         render(document.getElementById("root")!!) {
            browserRouter {
