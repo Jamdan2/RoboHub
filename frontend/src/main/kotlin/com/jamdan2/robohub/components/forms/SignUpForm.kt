b0e8d4cd718109ddd45f20fba6cc1f51fd0ffbba
Aidan Heller <asfheller@gmail.com>
2018-01-26 17:27:05 -0800

    Make forms looks somewhat normal

--- frontend/src/main/kotlin/com/jamdan2/robohub/components/forms/SignUpForm.kt
+++ frontend/src/main/kotlin/com/jamdan2/robohub/components/forms/SignUpForm.kt
@@ -1,8 +1,11 @@
 package com.jamdan2.robohub.components.forms
 
+import kotlinx.html.InputType
+import kotlinx.html.js.onClickFunction
 import org.w3c.dom.events.Event
 import react.*
 import react.dom.*
+import react.router.dom.routeLink
 
 class SignUpForm : RComponent<SignUpFormProps, RState>() {
     override fun RBuilder.render() {
@@ -19,38 +22,49 @@ class SignUpForm : RComponent<SignUpFormProps, RState>() {
                             label {
                                 +"First Name"
                             }
-                            input {}
+                            input(InputType.text) {}
                         }
                         div("right") {
                             label {
                                 +"Last Name"
                             }
-                            input {}
+                            input(InputType.text) {}
                         }
                     }
                     div("content") {
                         label {
                             +"Email"
                         }
-                        input {}
+                        input(InputType.text) {}
                     }
                     div("content") {
                         label {
                             +"Username"
                         }
-                        input {}
+                        input(InputType.text) {}
                     }
                     div("content") {
                         label {
                             +"Password"
                         }
-                        input {}
+                        input(InputType.password) {}
                     }
                     div("content") {
                         label {
                             +"Verify Password"
                         }
-                        input {}
+                        input(InputType.password) {}
+                    }
+                    div("content") {
+                        button(classes = "b-purple") {
+                            +"Submit"
+                        }
+                    }
+                    div("footer") {
+                        button(classes = "b-red") {
+                            attrs.onClickFunction = props.onCancelButtonClick
+                            +"Cancel"
+                        }
                     }
                 }
             }
