22a11ce0a285b11422cab2867cbac6230c913829
Aidan Heller <asfheller@gmail.com>
2018-01-29 21:52:06 -0800

    Nuke some stuff

--- frontend/src/main/kotlin/com/jamdan2/robohub/pages/WelcomePage.kt
+++ frontend/src/main/kotlin/com/jamdan2/robohub/pages/WelcomePage.kt
@@ -1,57 +1,20 @@
 package com.jamdan2.robohub.pages
 
-import com.jamdan2.robohub.components.forms.logInForm
-import com.jamdan2.robohub.components.forms.modal
-import com.jamdan2.robohub.components.forms.signUpForm
 import com.jamdan2.robohub.components.navigation.navigationBar
 import com.jamdan2.robohub.components.pageHeaders.welcomePageHeader
-import kotlinx.html.js.onClickFunction
 import react.*
 import react.dom.button
 
-class WelcomePage : RComponent<RProps, WelcomePageState>() {
+class WelcomePage : RComponent<RProps, RState>() {
     override fun RBuilder.render() {
-        modal(state.logInFormIsOpen) {
-            logInForm {
-                it.preventDefault()
-                setState {
-                    logInFormIsOpen = false
-                }
-            }
-        }
-        modal(state.signUpFormIsOpen) {
-            signUpForm {
-                it.preventDefault()
-                setState {
-                    signUpFormIsOpen = false
-                }
-            }
-        }
         navigationBar {
             button {
-                attrs.onClickFunction = {
-                    it.preventDefault()
-                    setState {
-                        signUpFormIsOpen = true
-                    }
-                }
                 +"Sign Up"
             }
             button {
-                attrs.onClickFunction = {
-                    it.preventDefault()
-                    setState {
-                        logInFormIsOpen = true
-                    }
-                }
                 +"Log In"
             }
         }
         welcomePageHeader("RoboHub", "Share your FRC robot with other teams")
     }
-}
-
-interface WelcomePageState : RState {
-    var logInFormIsOpen: Boolean
-    var signUpFormIsOpen: Boolean
 }
